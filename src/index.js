936e75f3cf77594d9072841b953cf017f9660afc
3600-abhi <abhishekjaiswal3600@gmail.com>
2023-06-13 08:54:56 +0530

    FEAT: Added Rate Limiting on APIs

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
-const express = require("express");
-const { ServerConfig } = require("./config");
-const apiRoutes = require("./routes");
+const express = require('express');
+const rateLimit = require('express-rate-limit');
+const { ServerConfig } = require('./config');
+const apiRoutes = require('./routes');
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/api", apiRoutes);
+
+const limiter = rateLimit({
+  windowMs: 2 * 60 * 1000, // 2 minutes
+  max: 3, // Limit each IP to 3 requests per `window` (here, per 3 minutes)
+});
+
+// Enabling the Rate limiter
+app.use(limiter);
+
+
+app.use('/api', apiRoutes);
 
 app.listen(ServerConfig.PORT, () => {
   console.log(`Successfully started the server at PORT : ${ServerConfig.PORT}`);
